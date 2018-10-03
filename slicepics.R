7622c7a00453b856d2b734d2f0e062595411fc6f
Alex Fish <acfishx@gmail.com>
2018-10-03 15:34:09 -0500

    Clean up .jpg subset script

--- slicepics.R
+++ slicepics.R
@@ -3,48 +3,48 @@ library(jpeg)
 #GLOBAL VARIABLES
 source_dir <- "C:/Users/Alex/Documents/UNL East Sorghum 8-17-18/"
 output_dir <- "C:/Users/Alex/Documents/UNL East Sorghum 8-17-18 SmallPics/"
-number_of_row_sections <- 5
-number_of_column_sections <- 2
+number_of_row_subsets <- 5
+number_of_column_subsets <- 2
 
 files <- list.files(path=source_dir, pattern="*", full.names=TRUE, recursive=FALSE)
 lapply(files, function(x) {
   
   #FILE NAME OF BASE PICTURE
-  basefilename = gsub('.{4}$','',basename(x))
+  base_file_name <- gsub('.{4}$','',basename(x))
   
   #LOAD PICTURE INTO R,G,B MATRICES
   jpg <- readJPEG(x)
   
   #SET UP SUBSETS OF PICTURE
-  rows=dim(jpg)[1]
-  cols=dim(jpg)[2]
+  rows <- dim(jpg)[1]
+  cols <- dim(jpg)[2]
   
-  rowSectionSize = rows / number_of_row_sections
-  colSectionSize = cols / number_of_column_sections
+  row_subset_size <- rows / number_of_row_subsets
+  column_subset_size <- cols / number_of_column_subsets
   
-  count = 0
+  count <- 0
   
   #DIVIDE PICTURE INTO NUMBER_OF_ROW_SECTIONS X NUMBER_OF_COLUMN_SECTIONS SUBSETS
-  for(rowIndex in c(0:(rowSections-1))) {
-    for(columnIndex in c(0:(colSections-1))) {
+  for(row_subset_index in c(0:(number_of_row_subsets-1))) {
+    for(col_subset_index in c(0:(number_of_column_subsets-1))) {
       
-      TopRow = rowSectionSize*rowIndex + 1
-      BottomRow = rowSectionSize*(rowIndex + 1)
+      top_row_index <- row_subset_size*row_subset_index + 1
+      bottom_row_index <- row_subset_size*(row_subset_index + 1)
       
-      LeftCol = colSectionSize*columnIndex + 1
-      RightCol = colSectionSize*(columnIndex + 1)
+      left_col_index <- column_subset_size*col_subset_index + 1
+      right_col_index <- column_subset_size*(col_subset_index + 1)
       
-      count = count + 1
+      count <- count + 1
       
       #CREATE PROPER FILENAME
-      filename = paste(basefilename, '_', count, '.jpg', sep='')
-      fulloutfile = paste(output_dir, filename)
+      outfile_name <- paste(base_file_name, '_', count, '.jpg', sep='')
+      full_path_outfile <- paste(output_dir, outfile_name)
       
       #SELECT SUBSET OF IMAGE
-      jpgDF = jpg[c(TopRow:BottomRow), c(LeftCol:RightCol), c(1:3)]
+      jpgDF <- jpg[c(top_row_index:bottom_row_index), c(left_col_index:right_col_index), c(1:3)]
       
       #SAVE PIC
-      writeJPEG(jpgDF, target=fulloutfile, quality=1.0)
+      writeJPEG(jpgDF, target=full_path_outfile, quality=1.0)
       
       #OPTIONAL LOGGING, CHANGE TO IF(TRUE) TO LOG
       if(FALSE) {
@@ -53,18 +53,18 @@ lapply(files, function(x) {
         print(x)
         print(basename(x))
         print(gsub('.{4}$','',basename(x)))
-        filename = gsub('.{4}$','',basename(x))
+        base_file_name = gsub('.{4}$','',basename(x))
         cat("Outfile ,", paste(filename, '_', count, '.jpg', sep=''))
         cat("Rows ", rows, "\n")
         cat("Cols ", cols, "\n")
-        cat("RowIndex ", rowIndex, "\n")
-        cat("columnIndex ", columnIndex, "\n")
-        cat("RowSectionSize ", rowSectionSize, "\n")
-        cat("ColSectionSize ", colSectionSize, "\n")
-        cat("TopRow ", TopRow, "\n")
-        cat("BottomRow ", BottomRow, "\n")
-        cat("LeftCol ", LeftCol, "\n")
-        cat("RightCol ", RightCol, "\n")
+        cat("RowIndex ", row_subset_index, "\n")
+        cat("col_subset_index ", col_subset_index, "\n")
+        cat("RowSectionSize ", row_subset_size, "\n")
+        cat("ColSectionSize ", column_subset_size, "\n")
+        cat("top_row_index ", top_row_index, "\n")
+        cat("bottom_row_index ", bottom_row_index, "\n")
+        cat("left_col_index ", left_col_index, "\n")
+        cat("right_col_index ", right_col_index, "\n")
       }
     }
   }
